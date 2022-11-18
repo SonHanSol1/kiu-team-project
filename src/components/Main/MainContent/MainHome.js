4115c4d6ab8a4d8e48aedf3db09ce2a3d6fd6fe2
SonHanSol1 <gksthf7869@naver.com>
2022-11-18 13:16:27 +0900

    aa

--- src/components/Main/MainContent/MainHome.js
+++ src/components/Main/MainContent/MainHome.js
@@ -1,7 +1,40 @@
-export default function MainHome(){
-    return(
-        <>
-            <h1>MainHome</h1>
-        </>
-    )
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Home from "./MainHome/Home";
+import NotLogin from "./MainHome/NotLogin";
+import Reservation from "./MainHome/Reservation";
+
+export default function MainHome() {
+  const [auth, setAuth] = useState();
+  const [reserve, setReserve] = useState(true);
+
+  useEffect(() => {
+    document.body.style.overflow = "auto";
+    axios
+      .get("/api/users/checkauthentication")
+      .then((res) => {
+        setAuth(true);
+      })
+      .catch((err) => {
+        // console.dir(err);
+        setAuth(false);
+      });
+  }, []);
+  return (
+    <>
+      <div>
+        {auth === false ? (
+          <NotLogin />
+        ) : auth === true ? (
+          reserve === false ? (
+            <Home />
+          ) : (
+            <Reservation />
+          )
+        ) : (
+          false
+        )}
+      </div>
+    </>
+  );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,21 @@ import thunk from "redux-thunk";
 
 import exampleReducer from "./modules/exampleSlice";
 import mainNavReducer from "./modules/mainNavSlice";
+import SearchMapBtnReducer from "./modules/SearchMapBtnSlice";
+import latlngReducer from "./modules/latlngSlice";
+import myinfoReducer from "./modules/myinfoSlice";
+import authUIReducer from "./modules/authUIStateSlice";
+
 
 export const store = configureStore({
   reducer: {
     //이름(변수와 비슷): 이름Reducer 규칙으로 작성
     example: exampleReducer,
-    mainNav: mainNavReducer
+    mainNav: mainNavReducer,
+    SearchMapBtn: SearchMapBtnReducer,
+    latlng:latlngReducer,
+    myinfo:myinfoReducer,
+    authUI:authUIReducer
   },
   middleware: [thunk]
 })
